3aae1190abbb0761aff3120f16744bd00849d2c9
Duc nguyen <nguyentienduc5@gmail.com>
2023-07-20 23:58:00 +0200

    Add density aware chamfer loss to replace emd

--- train.py
+++ train.py
@@ -15,9 +15,11 @@ import visdom
 from time import time
 sys.path.append("./emd/")
 import emd_module as emd
+from model_utils import calc_dcd
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--batchSize', type=int, default=8, help='input batch size')
+parser.add_argument('--batchSize', type=int, default=4, help='input batch size')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=12)
 parser.add_argument('--nepoch', type=int, default=25, help='number of epochs to train for')
 parser.add_argument('--model', type=str, default = '',  help='optional reload model path')
@@ -25,28 +27,40 @@ parser.add_argument('--num_points', type=int, default = 8192,  help='number of p
 parser.add_argument('--n_primitives', type=int, default = 16,  help='number of surface elements')
 parser.add_argument('--env', type=str, default ="MSN_TRAIN"   ,  help='visdom environment')
 parser.add_argument('--encoder', type=int, default =1   ,  help='which feature encoder to use 1=pointnet 2=pointnet++ 3=pointconv')
+parser.add_argument('--loss', type=int, default =2  ,  help='which loss to use 1=emd 2=dcd')
+
 
 
 opt = parser.parse_args()
 print (opt)
 
 class FullModel(nn.Module):
-    def __init__(self, model):
+    def __init__(self, model , dcd = False):
         super(FullModel, self).__init__()
         self.model = model
         self.EMD = emd.emdModule()
+        self.dcd = dcd
+        self.t_alpha = 200
+        self.n_lambda = 0.5
 
     def forward(self, inputs, gt, eps, iters):
         output1, output2, expansion_penalty = self.model(inputs)
         gt = gt[:, :, :3] 
         
-        dist, _ = self.EMD(output1, gt, eps, iters)
-        emd1 = torch.sqrt(dist).mean(1)
-        
-        dist, _ = self.EMD(output2, gt, eps, iters)
-        emd2 = torch.sqrt(dist).mean(1)    
+        if self.dcd:
+            dcd1, _, _ = calc_dcd(output1, gt, alpha=self.t_alpha, n_lambda=self.n_lambda)
+            dcd1 = dcd1.mean()
+            dcd2, _, _ = calc_dcd(output2, gt, alpha=self.t_alpha, n_lambda=self.n_lambda)
+            dcd2 = dcd2.mean()
+            return output1, output2, dcd1, dcd2 , expansion_penalty
 
-        return output1, output2, emd1, emd2, expansion_penalty
+        else:
+            dist, _ = self.EMD(output1, gt, eps, iters)
+            emd1 = torch.sqrt(dist).mean(1)
+            dist, _ = self.EMD(output2, gt, eps, iters)
+            emd2 = torch.sqrt(dist).mean(1)    
+        
+        return output1, output2, emd1, emd2 , expansion_penalty
 
 if __name__ == '__main__':
     vis = visdom.Visdom(port = 8097, env=opt.env) # set your port
@@ -125,10 +139,10 @@ if __name__ == '__main__':
             gt = gt.float().cuda()
             input = input.transpose(2,1).contiguous()
 
-            output1, output2, emd1, emd2, expansion_penalty  = network(input, gt.contiguous(), 0.005, 50)
-            loss_net = emd1.mean() + emd2.mean() + expansion_penalty.mean() * 0.1
-            loss_net.backward()
-            train_loss.update(emd2.mean().item())
+            output1, output2, emd_dcd1, emd_dcd2, expansion_penalty  = network(input, gt.contiguous(), 0.005, 50)
+            
+            loss_net = emd_dcd1.mean() + emd_dcd2.mean() + expansion_penalty.mean() * 0.1
+            train_loss.update(emd_dcd2.mean().item())
             optimizer.step()
 
             if i % 10 == 0:
@@ -162,8 +176,12 @@ if __name__ == '__main__':
                             markersize=2,
                             ),
                         )
-
-            print(opt.env + ' train [%d: %d/%d]  emd1: %f emd2: %f expansion_penalty: %f' %(epoch, i, len_dataset/opt.batchSize, emd1.mean().item(), emd2.mean().item(), expansion_penalty.mean().item()))
+            if opt.loss ==1:
+                print(opt.env + ' train [%d: %d/%d]  emd1: %f emd2: %f expansion_penalty: %f' %(epoch, i, len_dataset/opt.batchSize, emd_dcd1.mean().item(), emd_dcd2.mean().item(), expansion_penalty.mean().item()))
+            elif opt.loss ==2:
+                print(opt.env + ' train [%d: %d/%d]  dcd1: %f dcd2: %f expansion_penalty: %f' %(epoch, i, len_dataset/opt.batchSize, emd_dcd1.mean().item(), emd_dcd2.mean().item(), expansion_penalty.mean().item()))
+            else:
+                raise NotImplementedError("Not implemented yet")
         train_curve.append(train_loss.avg)
 
         # VALIDATION
@@ -208,8 +226,12 @@ if __name__ == '__main__':
                                 markersize=2,
                                 ),
                             )
-                    print(opt.env + ' val [%d: %d/%d]  emd1: %f emd2: %f expansion_penalty: %f' % (epoch, i, len(dataset_val) / opt.batchSize, emd1.mean().item(), emd2.mean().item(), expansion_penalty.mean().item()))
-
+                    if opt.loss ==1:
+                        print(opt.env + ' train [%d: %d/%d]  emd1: %f emd2: %f expansion_penalty: %f' %(epoch, i, len_dataset/opt.batchSize, emd_dcd1.mean().item(), emd_dcd2.mean().item(), expansion_penalty.mean().item()))
+                    elif opt.loss ==2:
+                        print(opt.env + ' train [%d: %d/%d]  dcd1: %f dcd2: %f expansion_penalty: %f' %(epoch, i, len_dataset/opt.batchSize, emd_dcd1.mean().item(), emd_dcd2.mean().item(), expansion_penalty.mean().item()))
+                    else:
+                        raise NotImplementedError("Not implemented yet")
         val_curve.append(val_loss.avg)
 
         vis.line(X=np.column_stack((np.arange(len(train_curve)),np.arange(len(val_curve)))),
@@ -220,7 +242,7 @@ if __name__ == '__main__':
                      Y=np.log(np.column_stack((np.array(train_curve),np.array(val_curve)))),
                      win='log',
                      opts=dict(title="log_emd", legend=["train_curve"+ opt.env, "val_curve"+ opt.env], markersize=2, ), )
-
+        
         log_table = {
           "train_loss" : train_loss.avg,
           "val_loss" : val_loss.avg,
